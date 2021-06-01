a42f2e28e15c3b8e76fb640cbe8a103565e00a3a
Varunkashyap2303 <r.varunkashyap99@gmail.com>
2021-06-01 22:09:21 +0530

    Added code to display images in the web app

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import redirect, render
+import os
+from django.shortcuts import render
 
 
 def index(request):
@@ -8,6 +9,8 @@ def index(request):
         from src.retrieval import search
 
         result_paths = search(request.POST.get("query"))
+        for i in range(len(result_paths)):
+            result_paths[i] = os.path.join("media","images", result_paths[i][49:])
         print(result_paths)
         return render(request, "app/results.html", {"paths": result_paths})
 
--- deploy/settings.py
+++ deploy/settings.py
@@ -120,6 +120,8 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "deploy", "static")]
+MEDIA_ROOT = os.path.join(BASE_DIR, "data")
+MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
