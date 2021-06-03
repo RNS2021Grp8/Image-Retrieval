8ea280df0ab68cfc29f4b725bdf8e9d13913beb8
anirudh-gowdar <anirudh.600@gmail.com>
2021-06-03 23:21:48 +0530

    Add update pkl button

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 import os
 from deploy.settings import BASE_DIR
 from src.image_processing import generate_embeddings
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from pathlib import Path
 
 def index(request):
@@ -11,16 +11,22 @@ def index(request):
     )
 
 def results(request):
+    pickle_file = os.path.join(BASE_DIR, "output.pkl")
 
     if request.method == "POST":
-
-        from src.retrieval import search
-        if not os.path.exists(os.path.join(BASE_DIR, "output.pkl")):
+        if "btn-search" in request.POST:
+            from src.retrieval import search
+            if not os.path.exists(pickle_file):
+                generate_embeddings()
+            result_paths = search(request.POST.get("query"))
+            for i in range(len(result_paths)):
+                filename = Path(result_paths[i]).name
+                result_paths[i] = os.path.join("media","images", filename)
+            print(result_paths)
+            return render(request, "app/results.html", {"paths": result_paths})
+        elif "btn-update" in request.POST:
+            if os.path.exists(pickle_file):
+                os.remove(pickle_file)
             generate_embeddings()
-        
-        result_paths = search(request.POST.get("query"))
-        for i in range(len(result_paths)):
-            filename = Path(result_paths[i]).name
-            result_paths[i] = os.path.join("media","images", filename)
-        print(result_paths)
-        return render(request, "app/results.html", {"paths": result_paths})
+            return redirect("app:index")
+            
